b1a1bac23d845752333a28c9c59dbd90e036df07
saiashish9 <saiashish3760@gmail.com>
2021-11-08 01:22:00 +0530

    k

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {View, StyleSheet} from 'react-native';
+
+const App = () => {
+  return <View style={styles.container} />;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+});
+
+export default App;
